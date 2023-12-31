010125dc68b9d60d14aa0d30168ae488eab65efd
JupiterG416 <127620272+JupiterG416@users.noreply.github.com>
2023-12-31 15:09:35 -0500

    Add files via upload

---
+++ DataScienceEcosystem-Copy1.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+# In this notebook, Data Science Tools and Ecosystem are summarized
+# 
+# ## Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. SQL
+# 3. Java
+# 4. R
+# 
+# ## Some of the commonly used libraries used by Data Scientists include:
+# 1. NumPy
+# 2. Pandas
+# 3. TensorFlow
+# 4. Matplotlib
+# 
+# 
+# | Data Science Tools   |
+# | -------------------- |
+# | NumPy                |
+# | Pandas               |
+# | Scikit-learn         |
+# 
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+#  `>`
+#  `<`
+#  `**`
+#  `*`
+#  `>=`
+#  `+`
+# 
+# 
+# ### This is a simple arithmetic expression to multiply then add integers.
+# result = (3 * 4) + 5
+# 
+# 
+# 
+# ### This will convert 200 minutes to hours by diving by 60.
+# hours = 400/60
+# 
+# 
+# # Some of the key takeaways from the course
+# <b> Objectives
+# * Python
+# * SQL
+# * Java
+# * R
+# 
+#     
+# ## AUTHOR
+#    VITO GIOVE
+
+# In[1]:
+
+
+hours = 400 / 60
+hours
+
+
+
+# In[ ]:
+
+
+
+
